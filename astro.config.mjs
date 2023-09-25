42c3c75531fe4c19c3374dfc59a912f19b2589a5
jeff4 <jeff@imac>
2023-09-24 23:15:49 -0700

    Added rss component; dynamic hydration for greeting on homepage with Preact installation and Greeting.jsx; night/dark toggle with just a script tag aka no framework JS needed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // value below should be the fully qualified URL	
+  site: "https://example.netlify.app",
+  integrations: [preact()]
+});
--- src/pages/_delete/README.md
+++
@@ -1,3 +0,0 @@
-All files in this directory work correctly. They are only "old" because
-they have static hard-coded values. They have not been refactored yet to
-use BaseLayout.astro. Ignore and delete these files.
