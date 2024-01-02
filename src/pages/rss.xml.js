b0f672bad58e64c346a9847b280770e616400d6f
jeff4 <jeff@imac>
2024-01-02 15:18:47 -0800

    catchup

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,17 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
+/*
 export async function GET(context) {
   return rss({
     title: 'Jeff Hwang | Blog',
     description: 'My journey learning Astro',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+*/
+//    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+//JH: complicated commenting out is because of the '*/' in line 10 above
+/*
     customData: `<language>en-us</language>`,
   });
 }
+
+*/
